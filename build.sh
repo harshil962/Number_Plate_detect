3fdb75e2f7c2ea0134cd10aa8c4983d893e95cc3
sneha296 <snehamandvani06@gmail.com>
2026-03-29 16:55:17 +0530

    fix deployment

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@ set -e
 
 pip install torch==2.1.0+cpu torchvision==0.16.0+cpu --extra-index-url https://download.pytorch.org/whl/cpu
 pip install -r requirements.txt
+python -c "import easyocr; easyocr.Reader(['en'], gpu=False)"  # pre-download models
 python manage.py collectstatic --no-input
 python manage.py migrate
