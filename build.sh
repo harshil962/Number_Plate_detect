5759d974ffcbed4a560546b60685ebbb5c5ddeb7
sneha296 <snehamandvani06@gmail.com>
2026-03-29 16:47:47 +0530

    fix deployment

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+set -e
+
+pip install torch==2.1.0+cpu torchvision==0.16.0+cpu --extra-index-url https://download.pytorch.org/whl/cpu
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+python manage.py collectstatic --no-input
 python manage.py migrate
